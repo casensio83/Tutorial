bfae86ba7dc5108ee0a12464222b2725afdd1bcc
cristina <casensio7@uoc.edu>
2023-04-15 20:28:21 +0200

    Get fragment views

--- app/src/main/java/es/casensio/tutorial/home/ListFragment.kt
+++ app/src/main/java/es/casensio/tutorial/home/ListFragment.kt
@@ -1,6 +1,26 @@
 package es.casensio.tutorial.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import es.casensio.tutorial.R
 
 class ListFragment: Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var errorMessage: TextView
+    private lateinit var progressBar: ProgressBar
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = view.findViewById(R.id.recycler_view)
+        errorMessage = view.findViewById(R.id.error_message)
+        progressBar = view.findViewById(R.id.progressbar)
+    }
 }
