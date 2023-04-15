d34d0123a8b174f036d29728b436daf9a6f9e7b1
cristina <casensio7@uoc.edu>
2023-04-15 21:33:51 +0200

    Repo model

---
+++ app/src/main/java/es/casensio/tutorial/model/Repo.kt
@@ -0,0 +1,14 @@
+package es.casensio.tutorial.model
+
+import com.squareup.moshi.Json
+
+data class Repo(
+    val id: Long,
+    val name: String,
+    val description: String,
+    val owner: User,
+    @Json(name = "stargazers_count")
+    val stars: Long,
+    @Json(name = "forks_count")
+    val forks: Long
+)
